60425bfb5699741959b6a55195fd631f19106054
Oleg Tsvetkov <olecve@gmail.com>
2026-05-25 15:37:34 +0300

    Expose formatWithConfig in cljfmt-js

--- src/cljfmt_js/core.cljs
+++ src/cljfmt_js/core.cljs
@@ -1,5 +1,6 @@
 (ns cljfmt-js.core
-  (:require [cljfmt.core :as cljfmt]))
+  (:require [cljfmt.core :as cljfmt]
+            [cljs.reader :as reader]))
 
 (defn format [source opts]
   (let [opts-clj (when (some? opts)
@@ -7,3 +8,10 @@
     (if (empty? opts-clj)
       (cljfmt/reformat-string source)
       (cljfmt/reformat-string source opts-clj))))
+
+(defn format-with-config [source config-edn]
+  (if (or (nil? config-edn) (= "" config-edn))
+    (cljfmt/reformat-string source)
+    (let [parsed (reader/read-string config-edn)
+          merged (merge cljfmt/default-options parsed)]
+      (cljfmt/reformat-string source merged))))
